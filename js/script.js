06e82034b92fb879a1b79fa346a0135d38168091
colbas93 <49678212+colbas93@users.noreply.github.com>
2021-05-11 16:43:19 +0300

    поправил js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-"use strict"
 document.addEventListener("DOMContentLoaded", function () {
+// Открытие/закрытие формы
+  if(document.querySelector('.js-booking')) {
+    const $book = document.querySelector('.js-booking');
+    const $bookForm = document.querySelector('.booking-form');
 
-  document.querySelector(".js-booking").addEventListener("click", function (e) {
-    e.preventDefault();
-    document
-      .querySelector(".booking-form")
-      .classList.toggle("booking-form--show");
-  });
-});
-
+    $book.addEventListener('click', function(e) {
+      e.preventDefault();
+      $bookForm.classList.toggle("booking-form--show");
+    });
+  }
+  
+  // Звездный рейтинг
   const ratings = document.querySelectorAll('.rating');
   if(ratings.length > 0) {
     initRatings();
   }
-  
-  // Основная функция
+
+  // основная функция
   function initRatings() {
     let ratingActive, ratingValue;
     // Бегаем по всем рейтингам на странице
@@ -22,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const rating = ratings[index];
       initRating(rating);
     }
-    // Инициализируем конкретный рейтинг
+    // онициализируем конкретный рейтинг
     function initRating(rating) {
       initRatingVars(rating);
       
@@ -33,32 +35,32 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
     
-    // Инициализация переменных
+    // инициализация переменных
     function initRatingVars(rating) {
       ratingActive = rating.querySelector('.rating__active');
       ratingValue = rating.querySelector('.rating__value');
     }
     
     
-    // Изменнение ширины активных звезд
+    // изменнение ширины активных звезд
     function setRatingActiveWidth(index = ratingValue.innerHTML) {
       const ratingActiveWidth = index / 0.1;
       ratingActive.style.width = `${ratingActiveWidth}%`;
     }
-    // Возможность указывать оценку
+    // возможность указывать оценку
     function setRating(rating) {
       const ratingItems = rating.querySelectorAll('.rating__item');
       for (let index = 0; index < ratingItems.length; index++) {
         const ratingItem = ratingItems[index];
         ratingItem.addEventListener('mouseenter', function(e) {
-          // Обновление переменной
+          // обновление переменной
           initRatingVars(rating);
-          // Обновление активных звезд
+          // обновление активных звезд
           setRatingActiveWidth(ratingItem.value);
         });
 
         ratingItem.addEventListener('mouseleave', function(e) {
-          // Обновление активных звезд
+          // обновление активных звезд
           setRatingActiveWidth();
         });
 
@@ -72,11 +74,51 @@ document.addEventListener("DOMContentLoaded", function () {
             setRatingActiveWidth();
           }
         });
-
-
       }
     }
-  }
+  };
+
+});
+
+// Jquery
+$(document).ready(function() {
+  // Счётчик
+  // создаём действие при нажатии
+	$('body').on('click', '.js-minus, .js-plus', function() {    
+		const $row = $(this).closest('.number'); // объявляем строку(весь блок)    
+		const $input = $row.find('.number-text'); // находим текстовое поле    
+		const step = $row.data('step'); // находим дата-атрибут с числом шага
+		let val = parseFloat($input.val()); // Приводим содержимое поля в числовое
+    // меняем значение в поле
+		if ($(this).hasClass('js-minus')) {
+			val -= step;
+		} else {
+			val += step;
+		}
+		$input.val(val); // перезаписываем значение в поле
+		$input.change();
+		return false;
+	});
+
+  // проверяем значение поля
+	$('body').on('change', '.number-text', function(){
+		var $input = $(this);
+		var $row = $input.closest('.number');
+		var step = $row.data('step'); // шаг
+		var min = parseInt($row.data('min')); // макс значение
+		var max = parseInt($row.data('max')); // мин значение
+		var val = parseInt($input.val()); // конвертирует в числовое значение
+    // проверка поля
+		if (isNaN(val)) {
+			val = step;
+		} else if (min && val < min) {
+			val = min;	
+		} else if (max && val > max) {
+			val = max;	
+		}
+		$input.val(val); // перезаписываем значение в поле
+	});
+});
     
 
   
